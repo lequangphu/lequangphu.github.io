9d7a283ce94758bfd0e493ac3aaeddd330e1b1ef
lequangphu <quangphu.le512@gmail.com>
2023-04-10 23:19:04 +0700

    6th commit

--- blog/posts/uniswap-stablecoin-depeg/resource/vol.sql
+++ blog/posts/uniswap-stablecoin-depeg/resource/vol.sql
@@ -33,12 +33,28 @@ with
       1 = 1
       AND BLOCK_TIMESTAMP BETWEEN '2023-03-07 15:00:00.000' AND '2023-03-16 17:00:00.000'
   ),
-  top_target AS (
+  top_vol AS (
     SELECT
-      symbol_out AS symbol,
+      symbol,
       sum(amount_usd) AS vol
     FROM
-      swap
+      (
+        SELECT
+          symbol_in AS symbol,
+          amount_usd
+        FROM
+          swap
+        WHERE
+          symbol_in != symbol_out
+        UNION ALL
+        SELECT
+          symbol_out,
+          amount_usd
+        FROM
+          swap
+        WHERE
+          symbol_in != symbol_out
+      )
     GROUP BY
       1
     ORDER BY
@@ -46,10 +62,52 @@ with
     LIMIT
       4
   ),
+  top_vol_by_dex AS (
+    SELECT
+      *
+    FROM
+      (
+        SELECT
+          dex,
+          symbol,
+          sum(amount_usd) AS vol,
+          rank() over (
+            partition by
+              dex
+            ORDER BY
+              vol DESC
+          ) AS rank
+        FROM
+          (
+            SELECT
+              dex,
+              symbol_in AS symbol,
+              amount_usd
+            FROM
+              swap
+            WHERE
+              symbol_in != symbol_out
+            UNION ALL
+            SELECT
+              dex,
+              symbol_out,
+              amount_usd
+            FROM
+              swap
+            WHERE
+              symbol_in != symbol_out
+          )
+        GROUP BY
+          1,
+          2
+      )
+    WHERE
+      rank <= 4
+  ),
   join_swap_stable AS (
     SELECT
       blocktime,
-      dex,
+      s.dex,
       period,
       amount_usd,
       s1.symbol IS NOT NULL AS from_stable,
@@ -61,22 +119,36 @@ with
         or to_stable THEN 'Stable ↔️ Non-stable'
         ELSE 'Non-stable ↔️ Non-stable'
       END AS pool,
-      t1.symbol IS NOT NULL AS from_top_target,
-      t2.symbol IS NOT NULL AS to_top_target,
+      t1.symbol IS NOT NULL AS from_top_vol,
+      t2.symbol IS NOT NULL AS to_top_vol,
       CASE
-        WHEN from_top_target THEN symbol_in
+        WHEN from_top_vol THEN symbol_in
         ELSE 'Other Coins'
       END symbol_in,
       CASE
-        WHEN to_top_target THEN symbol_out
+        WHEN to_top_vol THEN symbol_out
         ELSE 'Other Coins'
-      END symbol_out
+      END symbol_out,
+      td1.symbol IS NOT NULL AS from_top_vol_by_dex,
+      td2.symbol IS NOT NULL AS to_top_vol_by_dex,
+      CASE
+        WHEN from_top_vol_by_dex THEN symbol_in
+        ELSE 'Other Coins'
+      END symbol_in_by_dex,
+      CASE
+        WHEN to_top_vol_by_dex THEN symbol_out
+        ELSE 'Other Coins'
+      END symbol_out_by_dex
     FROM
       swap AS s
       LEFT JOIN stable AS s1 ON s1.symbol = s.symbol_in
       LEFT JOIN stable AS s2 ON s2.symbol = s.symbol_out
-      LEFT JOIN top_target AS t1 ON t1.symbol = s.symbol_in
-      LEFT JOIN top_target AS t2 ON t2.symbol = s.symbol_out
+      LEFT JOIN top_vol AS t1 ON t1.symbol = s.symbol_in
+      LEFT JOIN top_vol AS t2 ON t2.symbol = s.symbol_out
+      LEFT JOIN top_vol_by_dex AS td1 ON td1.symbol = s.symbol_in
+      AND td1.dex = s.dex
+      LEFT JOIN top_vol_by_dex AS td2 ON td2.symbol = s.symbol_out
+      AND td2.dex = s.dex
   )
 SELECT
   blocktime,
@@ -87,6 +159,8 @@ SELECT
   to_stable,
   symbol_in,
   symbol_out,
+  symbol_in_by_dex,
+  symbol_out_by_dex,
   sum(amount_usd) AS vol
 FROM
   join_swap_stable
@@ -98,4 +172,6 @@ GROUP BY
   5,
   6,
   7,
-  8
+  8,
+  9,
+  10
